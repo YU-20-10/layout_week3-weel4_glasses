9c3c15fa3b9943113c288e9e86c23abffe784401
YU-20-10 <108886846+YU-20-10@users.noreply.github.com>
2024-08-13 20:43:42 +0800

    fix config

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: '/web-layout-training-vite/',
+  base: '/layout_week3-weel4_glasses/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
